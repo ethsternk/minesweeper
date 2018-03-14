b35e6e69444bcadd759d7a750d88ef3e32d7882e
Ethan Sternke <ethan.sternke@gmail.com>
2018-03-14 15:49:12 -0400

    improved mine counting and added cover up for tiles

--- script.js
+++ script.js
@@ -4,22 +4,25 @@ const boardArray = [];
 var board = {
     width: 10,
     height: 10,
-    minePerc: 0.1,
-    mines: 1,
-
+    mines: 20,
+    
     construct: function () {
+        let possibleCells = [];
+        for (let i = 0; i < this.width * this.height; i++) {
+            if (i < this.width * this.height - this.mines) {
+                possibleCells.push(" ");
+            } else {
+                possibleCells.push("M");
+            }
+        }        
         for (let i = 0; i < this.height; i++) {
             boardArray.push([]);
-            for (let y = 0; y < this.width; y++) {
-                if (Math.random() > this.minePerc) {
-                    boardArray[i].push(" ");
-                } else {
-                    boardArray[i].push("M");
-                } 
+            for (let y = 0; y < this.width; y++) {                
+                boardArray[i].push(possibleCells.splice(Math.floor(Math.random() * possibleCells.length), 1).toString());
             }
         }
     },
-
+    
     draw: function () {
         for (let i = 0; i < this.height; i++) {
             const row = document.createElement("div");
@@ -27,13 +30,39 @@ var board = {
             container.appendChild(row);
             for (let y = 0; y < this.width; y++) {
                 const cell = document.createElement("div");
-                if (Math.random() > this.minePerc) {
-                    cell.className = "cell";
+
+                // let surroundingMines = 0;
+                // if (boardArray[i][y] === " ") {
+                //     if (boardArray[i-1][y-1]) { surroundingMines++ };
+                //     if (boardArray[i-1][y]) { surroundingMines++ };
+                //     if (boardArray[i-1][y+1]) { surroundingMines++ };
+                //     if (boardArray[i][y-1]) { surroundingMines++ };
+                //     if (boardArray[i][y+1]) { surroundingMines++ };
+                //     if (boardArray[i+1][y-1]) { surroundingMines++ };
+                //     if (boardArray[i+1][y]) { surroundingMines++ };
+                //     if (boardArray[i+1][y+1]) { surroundingMines++ };
+                // }
+
+                // cell.innerHTML = surroundingMines;
+
+                if (boardArray[i][y] === " ") {
+                    cell.className = "cell closed";
                 } else {
-                    cell.className = "cell mine";
+                    cell.className = "cell mine closed";
                 }
+                cell.addEventListener("click", this);
                 row.appendChild(cell);
             }
+        }
+    },
+
+    handleEvent: function (event) {
+        let target = event.target;
+        console.log(target);
+        target.classList.remove("closed");
+
+        if (target.classList.contains("mine")) {
+            setTimeout(function(){ alert("welp"); }, 50);
         }
     }
 
