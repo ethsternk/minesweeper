cf2ef9572f603113c68b28312cc9ec0a50ba82d6
Ethan Sternke <ethan.sternke@gmail.com>
2018-03-16 15:04:54 -0400

    refactored a bit and fixed some bugs

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 const container = document.getElementById("board");
 let cells = document.getElementsByClassName("cell");
+let possibleCells = [];
 let boardArray = [];
-let flagCount = 15;
+let gameLive = false;
 let timer = 0;
-let gameBegun = false;
 
 var board = {
     width: 10,
     height: 10,
     mines: 15,
 
-    buildArray: function () {
-        let possibleCells = [];
+    // fills the dummy array (possibleCells) with all possible cells to be added
+    createPossibleCells: function () {
+        possibleCells = [];
         for (let i = 0; i < this.width * this.height; i++) {
             if (i < (this.width * this.height) - this.mines) {
                 possibleCells.push(" ");
@@ -19,179 +20,168 @@ var board = {
                 possibleCells.push("M");
             }
         }
-        for (let rowIndex = 0; rowIndex < (this.height + 2); rowIndex++) {
+    },
+
+    // pushes each cell from the dummy array (possibleCells) randomly into the real array (boardArray)
+    buildArray: function () {
+        for (let x = 0; x < this.height + 2; x++) {
             boardArray.push([]);
-            for (let colIndex = 0; colIndex < (this.width + 2); colIndex++) {
-                if (rowIndex >= 1 && rowIndex < this.height + 1 && colIndex >= 1 && colIndex < this.width + 1) {
-                    boardArray[rowIndex].push(possibleCells.splice(Math.floor(Math.random() * possibleCells.length), 1)[0]);
+            for (let y = 0; y < this.width + 2; y++) {
+                if (x >= 1 && x < this.height + 1 && y >= 1 && y < this.width + 1) {
+                    boardArray[x].push(possibleCells.splice(Math.floor(Math.random() * possibleCells.length), 1)[0]);
                 } else {
-                    boardArray[rowIndex][colIndex] = "-";
+                    boardArray[x][y] = "-";
                 }
             }
         }
     },
 
-    insertNumbersIntoArray: function () {
-        for (let i = 1; i < this.height + 1; i++) {
+    // changes empty cells in boardArray to numbers based on the # of surrounding mines
+    insertNumbers: function () {
+        for (let x = 1; x < this.height + 1; x++) {
             for (let y = 1; y < this.width + 1; y++) {
-                if (boardArray[i][y] === " ") {
+                if (boardArray[x][y] === " ") {
                     let surroundingMines = 0;
-                    if (boardArray[i - 1][y - 1] === "M") {
-                        surroundingMines++
-                    };
-                    if (boardArray[i - 1][y] === "M") {
-                        surroundingMines++
-                    };
-                    if (boardArray[i - 1][y + 1] === "M") {
-                        surroundingMines++
-                    };
-                    if (boardArray[i][y - 1] === "M") {
-                        surroundingMines++
-                    };
-                    if (boardArray[i][y + 1] === "M") {
-                        surroundingMines++
-                    };
-                    if (boardArray[i + 1][y - 1] === "M") {
-                        surroundingMines++
-                    };
-                    if (boardArray[i + 1][y] === "M") {
-                        surroundingMines++
-                    };
-                    if (boardArray[i + 1][y + 1] === "M") {
-                        surroundingMines++
-                    };
-                    boardArray[i][y] = surroundingMines;
+                    if (boardArray[x - 1][y - 1] === "M") { surroundingMines++ };
+                    if (boardArray[x - 1][y] === "M") { surroundingMines++ };
+                    if (boardArray[x - 1][y + 1] === "M") { surroundingMines++ };
+                    if (boardArray[x][y - 1] === "M") { surroundingMines++ };
+                    if (boardArray[x][y + 1] === "M") { surroundingMines++ };
+                    if (boardArray[x + 1][y - 1] === "M") { surroundingMines++ };
+                    if (boardArray[x + 1][y] === "M") { surroundingMines++ };
+                    if (boardArray[x + 1][y + 1] === "M") { surroundingMines++ };
+                    boardArray[x][y] = surroundingMines;
                 }
             }
         }
     },
 
+    // draws a new board in the HTML based on boardArray
     drawHtml: function () {
-        for (let i = 1; i < this.height + 1; i++) {
+        for (let x = 1; x < this.height + 1; x++) {
             const row = document.createElement("div");
             row.className = "row";
             container.appendChild(row);
             for (let y = 1; y < this.width + 1; y++) {
                 const cell = document.createElement("div");
-                if (boardArray[i][y] === "M") {
+                if (boardArray[x][y] === "M") {
                     cell.className = "cell mine closed";
                 } else {
-                    cell.className = "cell closed m" + boardArray[i][y];
+                    cell.className = "cell closed m" + boardArray[x][y];
                 }
-                cell.id = i + "-" + y;
+                cell.id = x + "-" + y;
                 cell.addEventListener("click", this.handleLeftClick);
                 cell.addEventListener("contextmenu", this.handleRightClick, false);
                 row.appendChild(cell);
             }
         }
-        flagCount = board.mines;
         timer = 0;
         document.getElementById("timer").innerHTML = timer;
-        document.getElementById("flagCount").innerHTML = flagCount
+        board.updateFlagCount();
     },
 
+    // unmasks cell
     handleLeftClick: function (event) {
-        gameBegun = true;
         let target = event.target;
-        cells = document.getElementsByClassName("cell");
+        gameLive = true;
         if (target.classList.contains("closed")) {
             target.classList.remove("closed");
+            // if the cell is empty, clear all neighbors too
             if (target.classList.contains("m0")) {
                 let pos = target.id.split("-");
-
-                function posChange(down, right) {
-                    return (Number(pos[0]) + down) + "-" + (Number(pos[1]) + right);
+                function clearNewCell(down, right) {
+                    document.getElementById( (Number(pos[0]) + down) + "-" + (Number(pos[1]) + right) ).click();
                 }
-                if (pos[0] < board.height) {
-                    document.getElementById(posChange(1, 0)).click();
-                }
-                if (pos[0] > 1) {
-                    document.getElementById(posChange(-1, 0)).click();
-                }
-                if (pos[1] < board.width) {
-                    document.getElementById(posChange(0, 1)).click();
-                }
-                if (pos[1] > 1) {
-                    document.getElementById(posChange(0, -1)).click();
-                }
+                if (pos[0] < board.height) { clearNewCell(1, 0); }
+                if (pos[0] > 1) { clearNewCell(-1, 0); }
+                if (pos[1] < board.width) { clearNewCell(0, 1); }
+                if (pos[1] > 1) { clearNewCell(0, -1); }
             }
-        }
-        cells = document.getElementsByClassName("cell");
-        if (target.classList.contains("mine")) {
-            target.classList.add("dead");
-            for (let i = 0; i < cells.length; i++) {
-                cells[i].classList.remove("closed");
-            }
-            gameBegun = false;
-            // setTimeout(function () {
-            //     alert("You lose!");
-            // }, 50);
-        } else {
-            let minesRemaining = 0;
-            let safeRemaining = 0;
-            for (let i = 0; i < cells.length; i++) {
-                if (cells[i].classList.contains("mine") && cells[i].classList.contains("closed") && cells[i].classList.contains("flag") === false) {
-                    minesRemaining++;
-                }
-                if (cells[i].classList.contains("mine") === false && (cells[i].classList.contains("closed") || cells[i].classList.contains("flag"))) {
-                    safeRemaining++;
-                }
-            }
-            if (safeRemaining < 1) {
+            // if the cell is a mine, you lose
+            if (target.classList.contains("mine")) {
+                target.classList.add("dead");
                 for (let i = 0; i < cells.length; i++) {
-                    if (cells[i].classList.contains("flag") === false) {
-                        cells[i].classList.remove("closed");
-                    }
+                    cells[i].classList.remove("closed");
                 }
-                gameBegun = false;
-                // setTimeout(function () {
-                //     alert("You win!");
-                // }, 50);
+                gameLive = false;
+                setTimeout(function () {
+                    alert("You lose!");
+                }, 50);
             }
         }
+        board.checkForWin();
     },
 
+    // adds or removes flag from cell
     handleRightClick: function (event) {
         let target = event.target;
         event.preventDefault();
         if (target.classList.contains("closed")) {
             target.classList.remove("closed");
             target.classList.add("flag");
-            flagCount--;
-            document.getElementById("flagCount").innerHTML = flagCount;
+            board.mines--;
+            board.updateFlagCount();
         } else if (target.classList.contains("flag")) {
             target.classList.remove("flag");
             target.classList.add("closed");
-            flagCount++;
-            document.getElementById("flagCount").innerHTML = flagCount;
+            board.mines++;
+            board.updateFlagCount();
         }
         return false;
     },
 
+    updateFlagCount: function () {
+        document.getElementById("flagCount").innerHTML = board.mines;
+    },
+
+    // checks if you've won
+    checkForWin: function () {
+        let safeRemaining = 0;
+        for (let i = 0; i < cells.length; i++) {
+            if (cells[i].classList.contains("mine") === false && (cells[i].classList.contains("closed") || cells[i].classList.contains("flag"))) {
+                safeRemaining++;
+            }
+        }
+        if (safeRemaining < 1 && gameLive) {
+            for (let i = 0; i < cells.length; i++) {
+                cells[i].classList.remove("closed");
+                cells[i].classList.remove("flag");
+            }
+            gameLive = false;
+            setTimeout(function () { alert("You win!"); }, 50);
+        }
+    }
+
 }
 
+// resets the board with new values
 function reset() {
     boardArray = [];
     container.innerHTML = "";
     board.width = Number(document.getElementById("width").value);
     board.height = Number(document.getElementById("height").value);
     board.mines = Number(document.getElementById("mines").value);
+    board.createPossibleCells();
     board.buildArray();
-    board.insertNumbersIntoArray();
+    board.insertNumbers();
     board.drawHtml();
     console.table(boardArray);
 }
 
+// runs timer
 setInterval(function () {
-    if (gameBegun) {
+    if (gameLive) {
         timer++;
         document.getElementById("timer").innerHTML = timer;
     }
 }, 1000);
 
-document.getElementById("start").addEventListener("click", reset)
+// adds click listener to the reset button
+document.getElementById("start").addEventListener("click", reset);
 
+// builds default board upon page load
+board.createPossibleCells();
 board.buildArray();
-board.insertNumbersIntoArray();
+board.insertNumbers();
 board.drawHtml();
 console.table(boardArray);
